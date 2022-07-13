b4ec4083965e55c0cd810b6ee4fb846e062bb04e
D1eku <dieku25ikari@gmail.com>
2022-07-13 03:05:40 -0400

    Update settings.py

--- API/env2/medicionesAPIProject/medicionesAPIProject/settings.py
+++ API/env2/medicionesAPIProject/medicionesAPIProject/settings.py
@@ -53,7 +53,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'medicionesAPIProject.urls'
 
